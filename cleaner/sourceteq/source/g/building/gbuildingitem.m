c49ef689ff86289b66c9602635033861bf9a9f7f
vauxhall <zero.griffin@gmail.com>
2016-03-31 10:15:27 -0600

    model area

--- cleaner/sourceteq/source/g/building/gbuildingitem.m
+++ cleaner/sourceteq/source/g/building/gbuildingitem.m
@@ -6,11 +6,6 @@
 {
     self = [super init];
     self.model = model;
-    
-    self.x = model.x;
-    self.y = model.y;
-    self.width = model.width;
-    self.height = model.height;
     self.color = GLKVector4Make(1, 1, 1, 0);
     self.image.srgb = YES;
     [self.image loadtextures:@[model.assetname]];
--- cleaner/sourceteq/source/m/area/marea.m
+++ cleaner/sourceteq/source/m/area/marea.m
@@ -2,4 +2,25 @@
 
 @implementation marea
 
+-(instancetype)init
+{
+    self = [super init];
+    
+    CGFloat rawscreenwidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat rawscreenheight = [UIScreen mainScreen].bounds.size.height;
+    
+    if(rawscreenwidth > rawscreenheight)
+    {
+        self.screenwidth = rawscreenwidth;
+        self.screenheight = rawscreenheight;
+    }
+    else
+    {
+        self.screenwidth = rawscreenheight;
+        self.screenheight = rawscreenwidth;
+    }
+    
+    return self;
+}
+
 @end
