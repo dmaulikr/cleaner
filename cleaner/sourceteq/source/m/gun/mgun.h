5fcf734c43b26c674b23dc51b0a5a34086f54ab9
vauxhall <zero.griffin@gmail.com>
2016-04-01 17:23:30 -0600

    assets gun

--- cleaner/sourceteq/source/m/gun/mgun.h
+++ cleaner/sourceteq/source/m/gun/mgun.h
@@ -1,11 +1,15 @@
 #import "appdel.h"
 
 @class marea;
+@class ggunpointer;
+@class gguntarget;
 
 @interface mgun:NSObject
 
 -(instancetype)init:(marea*)modelarea;
 
 @property(weak, nonatomic)marea *modelarea;
+@property(strong, nonatomic)gguntarget *spatialtarget;
+@property(strong, nonatomic)ggunpointer *spatialpointer;
 
 @end
--- cleaner/sourceteq/source/m/gun/mgun.m
+++ cleaner/sourceteq/source/m/gun/mgun.m
@@ -6,6 +6,8 @@
 {
     self = [super init];
     self.modelarea = modelarea;
+    self.spatialpointer = [[ggunpointer alloc] init];
+    self.spatialtarget = [[gguntarget alloc] init];
     
     return self;
 }
