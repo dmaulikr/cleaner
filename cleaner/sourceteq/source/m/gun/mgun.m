426c09cc310c43fbcf6f884d057f5ac36ff5a491
vauxhall <zero.griffin@gmail.com>
2016-04-01 19:28:26 -0600

    gun moving

--- cleaner/sourceteq/source/m/gun/mgun.m
+++ cleaner/sourceteq/source/m/gun/mgun.m
@@ -7,12 +7,14 @@ typedef NS_ENUM(NSUInteger, delta_move)
     delta_move_none
 };
 
+static NSInteger const deltaspeed = 3;
+
 @interface mgun ()
 
-@property(nonatomic)NSUInteger expectedx;
-@property(nonatomic)NSUInteger expectedy;
 @property(nonatomic)delta_move deltahr;
 @property(nonatomic)delta_move deltavr;
+@property(nonatomic)NSInteger expectedx;
+@property(nonatomic)NSInteger expectedy;
 
 @end
 
@@ -24,7 +26,11 @@ typedef NS_ENUM(NSUInteger, delta_move)
     self.modelarea = modelarea;
     self.spatialpointer = [[ggunpointer alloc] init];
     self.spatialtarget = [[gguntarget alloc] init];
-    [self innermovetox:modelarea.centerx y:modelarea.centery];
+    self.deltahr = delta_move_none;
+    self.deltavr = delta_move_none;
+    self.expectedx = modelarea.centerx;
+    self.expectedy = modelarea.centery;
+    [self innermovetox:self.expectedx y:self.expectedy];
     [self.spatialtarget rasterize];
     [self.spatialpointer rasterize];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notifiedglkmove:) name:notification_glkmove object:nil];
@@ -42,21 +48,64 @@ typedef NS_ENUM(NSUInteger, delta_move)
 -(void)notifiedglkmove:(NSNotification*)notification
 {
     BOOL edited = NO;
+    NSInteger newx = 0;
+    NSInteger newy = 0;
     
     if(self.deltahr != delta_move_none)
     {
         edited = YES;
+        
+        if(self.deltahr == delta_move_add)
+        {
+            newx = self.x += deltaspeed;
+            
+            if(newx > self.expectedx)
+            {
+                newx = self.expectedx;
+                self.deltahr = delta_move_none;
+            }
+        }
+        else
+        {
+            newx = self.x -= deltaspeed;
+            
+            if(newx < self.expectedx)
+            {
+                newx = self.expectedx;
+                self.deltahr = delta_move_none;
+            }
+        }
     }
     
     if(self.deltavr != delta_move_none)
     {
         edited = YES;
+        
+        if(self.deltavr == delta_move_add)
+        {
+            newy = self.y += deltaspeed;
+            
+            if(newy > self.expectedy)
+            {
+                newy = self.expectedy;
+                self.deltavr = delta_move_none;
+            }
+        }
+        else
+        {
+            newy = self.y -= deltaspeed;
+            
+            if(newy < self.expectedy)
+            {
+                newy = self.expectedy;
+                self.deltavr = delta_move_none;
+            }
+        }
     }
     
     if(edited)
     {
-        [self.spatialpointer render];
-        [self.spatialtarget render];
+        [self innermovetox:newx y:newy];
     }
     
     [self.spatialpointer movetotop];
@@ -67,8 +116,17 @@ typedef NS_ENUM(NSUInteger, delta_move)
 
 -(void)innermovetox:(NSInteger)x y:(NSInteger)y
 {
+    BOOL focused = NO;
+    
+    if(self.deltavr == delta_move_none && self.deltahr == delta_move_none)
+    {
+        focused = YES;
+    }
+    
     self.x = x;
     self.y = y;
+    [self.spatialpointer centeronx:x y:y focused:focused];
+    [self.spatialtarget centeronx:x y:y focused:focused];
 }
 
 #pragma mark public
@@ -77,8 +135,34 @@ typedef NS_ENUM(NSUInteger, delta_move)
 {
     NSInteger deltax = self.x - x;
     NSInteger deltay = self.y - y;
+    self.expectedx = x;
+    self.expectedy = y;
     
+    if(deltax > 0)
+    {
+        self.deltahr = delta_move_add;
+    }
+    else if(deltax < 0)
+    {
+        self.deltahr = delta_move_subs;
+    }
+    else
+    {
+        self.deltahr = delta_move_none;
+    }
     
+    if(deltay > 0)
+    {
+        self.deltavr = delta_move_add;
+    }
+    else if(deltay < 0)
+    {
+        self.deltavr = delta_move_subs;
+    }
+    else
+    {
+        self.deltavr = delta_move_none;
+    }
 }
 
 @end
