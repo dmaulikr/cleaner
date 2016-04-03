83fca493f503277cffdd3e763a3ff3318e046a07
vauxhall <zero.griffin@gmail.com>
2016-04-03 18:22:42 -0500

    model gun shot

---
+++ cleaner/sourceteq/source/m/gun/mgunshot.m
@@ -0,0 +1,13 @@
+//
+//  mgunshot.m
+//  cleaner
+//
+//  Created by zero on 4/3/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mgunshot.h"
+
+@implementation mgunshot
+
+@end
