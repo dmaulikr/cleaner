398f32f00041848e1864eb2269dcf513f3e1c52a
vauxhall <zero.griffin@gmail.com>
2016-04-04 17:37:13 -0500

    model effect item

--- cleaner/sourceteq/source/m/effect/meffectitem.h
+++ cleaner/sourceteq/source/m/effect/meffectitem.h
@@ -1,7 +1,13 @@
 #import <UIKit/UIKit.h>
 
+@class meffect;
+@class geffect;
+
 @interface meffectitem:NSObject
 
+-(instancetype)init:(meffect*)model x:(CGFloat)x y:(CGFloat)y;
+
+@property(weak, nonatomic)meffect *model;
 @property(strong, nonatomic)geffect *spatial;
 @property(copy, nonatomic)NSString *assetname;
 
--- cleaner/sourceteq/source/m/effect/meffectitem.m
+++ cleaner/sourceteq/source/m/effect/meffectitem.m
@@ -1,6 +1,38 @@
 #import "meffectitem.h"
+#import "appdel.h"
 
 @implementation meffectitem
 
+-(instancetype)init:(meffect*)model x:(CGFloat)x y:(CGFloat)y
+{
+    self = [super init];
+    self.model = model;
+    self.spatial = [[geffect alloc] init:self];
+    self.spatial.x = x;
+    self.spatial.y = y;
+    
+    return self;
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark notified
+
+-(void)notifiedglkmove:(NSNotification*)notification
+{
+    
+}
+
+#pragma mark public
+
+-(void)rasterize
+{
+    [self.spatial rasterize];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notifiedglkmove:) name:notification_glkmove object:nil];
+}
 
 @end
