01fea7aad2bb2e235aab67c37960540fc691c8e3
vauxhall <zero.griffin@gmail.com>
2016-04-04 19:00:22 -0500

    worked fix for texture

--- cleaner/sourceteq/source/g/base/gspatialtexture.m
+++ cleaner/sourceteq/source/g/base/gspatialtexture.m
@@ -35,20 +35,18 @@
 
 -(void)draw:(GLKBaseEffect*)effect
 {
-    if(self.image.loading)
+    if(!self.image.loading)
     {
-        NSLog(@"shit");
+        effect.texture2d0.enabled = YES;
+        effect.texture2d0.envMode = GLKTextureEnvModeReplace;
+        effect.texture2d0.name = self.image.current;
+        glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+        glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 0, self.pointertexture);
+        
+        [super draw:effect];
+        
+        glDisableVertexAttribArray(GLKVertexAttribTexCoord0);
     }
-    
-    effect.texture2d0.enabled = YES;
-    effect.texture2d0.envMode = GLKTextureEnvModeReplace;
-    effect.texture2d0.name = self.image.current;
-    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 0, self.pointertexture);
-    
-    [super draw:effect];
-    
-    glDisableVertexAttribArray(GLKVertexAttribTexCoord0);
 }
 
 @end
