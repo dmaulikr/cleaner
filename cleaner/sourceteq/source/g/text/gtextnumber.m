56dfb098bc569d9f0a39ad6be44e40482187a627
vauxhall <zero.griffin@gmail.com>
2016-04-05 12:05:30 -0500

    glyphs

--- cleaner/sourceteq/source/g/text/gtextnumber.m
+++ cleaner/sourceteq/source/g/text/gtextnumber.m
@@ -1,5 +1,19 @@
 #import "gtextnumber.h"
 
+static NSInteger const glyphwidth = 24;
+static NSInteger const glyphheight = 34;
+static NSInteger const marginhr = 6;
+static NSInteger const marginvr = 7;
+
 @implementation gtextnumber
+
+-(instancetype)init
+{
+    self = [super init];
+    self.width = glyphwidth;
+    self.height = glyphheight;
+    
+    return self;
+}
 
 @end
