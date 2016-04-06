868dc43f46f5f9b28c86948940d46cd173d3ce01
vauxhall <zero.griffin@gmail.com>
2016-04-06 14:16:24 -0500

    mode life

--- cleaner/sourceteq/source/g/hub/ghublifebar.m
+++ cleaner/sourceteq/source/g/hub/ghublifebar.m
@@ -1,5 +1,18 @@
 #import "ghublifebar.h"
 
+static CGFloat const red = 1;
+static CGFloat const green = 1;
+static CGFloat const blue = 1;
+static CGFloat const alpha = 0.4;
+
 @implementation ghublifebar
+
+-(instancetype)init
+{
+    self = [super init:[mcolor red:red green:green blue:blue alpha:alpha]];
+    [self rasterize];
+    
+    return self;
+}
 
 @end
--- cleaner/sourceteq/source/m/game/mgamehublife.m
+++ cleaner/sourceteq/source/m/game/mgamehublife.m
@@ -1,12 +1,13 @@
 #import "mgamehublife.h"
 
-static NSInteger const initialamount = 100;
+static NSInteger const maxlife = 100;
 static NSInteger const lifex = 20;
 static NSInteger const lifey = 20;
 static NSInteger const lifewidth = 200;
 static NSInteger const lifeheight = 20;
-static NSInteger const lifemarginhr = 2;
-static NSInteger const lifemarginvr = 2;
+static NSInteger const lifemarginleft = 20;
+static NSInteger const lifemarginright = 10;
+static NSInteger const lifemarginvr = 3;
 
 @implementation mgamehublife
 {
@@ -18,10 +19,23 @@ static NSInteger const lifemarginvr = 2;
 {
     self = [super init];
     self.model = model;
-    self.amount = initialamount;
-    self.changed = NO;
+    self.amount = maxlife;
+    self.changed = YES;
     maxlifeheight = lifeheight - (lifemarginvr + lifemarginvr);
-    maxlifewidth = lifewidth - (lifemarginhr + lifemarginhr);
+    maxlifewidth = lifewidth - (lifemarginleft + lifemarginright);
+    
+    self.spatiallife = [[ghublife alloc] init];
+    self.spatiallife.x = lifex;
+    self.spatiallife.y = lifey;
+    self.spatiallife.width = lifewidth;
+    self.spatiallife.height = lifeheight;
+    [self.spatiallife rasterize];
+    
+    self.spatiallifebar = [[ghublifebar alloc] init];
+    self.spatiallifebar.x = lifex + lifemarginleft;
+    self.spatiallifebar.y = lifey + lifemarginvr;
+    self.spatiallifebar.height = maxlifeheight;
+    [self reposlife];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notifiedglkmove:) name:notification_glkmove object:nil];
     
@@ -48,7 +62,9 @@ static NSInteger const lifemarginvr = 2;
 
 -(void)reposlife
 {
-    
+    CGFloat percent = self.amount / (CGFloat)maxlife;
+    NSInteger lifewidth = percent * maxlifewidth;
+    self.spatiallifebar.width = lifewidth;
 }
 
 @end
