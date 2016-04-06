9398ffbadc26b9ac6e996e4d33b657483b63cb9c
vauxhall <zero.griffin@gmail.com>
2016-04-06 13:15:01 -0500

    model life

--- cleaner/sourceteq/source/m/game/mgamehublife.h
+++ cleaner/sourceteq/source/m/game/mgamehublife.h
@@ -1,12 +1,17 @@
 #import "appdel.h"
 
 @class mgame;
+@class ghublife;
+@class ghublifebar;
 
 @interface mgamehublife:NSObject
 
 -(instancetype)init:(mgame*)model;
 
 @property(weak, nonatomic)mgame *model;
+@property(strong, nonatomic)ghublife *spatiallife;
+@property(strong, nonatomic)ghublifebar *spatiallifebar;
 @property(nonatomic)NSInteger amount;
+@property(nonatomic)BOOL changed;
 
 @end
--- cleaner/sourceteq/source/m/game/mgamehublife.m
+++ cleaner/sourceteq/source/m/game/mgamehublife.m
@@ -9,8 +9,27 @@ static NSInteger const initialamount = 100;
     self = [super init];
     self.model = model;
     self.amount = initialamount;
+    self.changed = NO;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notifiedglkmove:) name:notification_glkmove object:nil];
     
     return self;
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark notified
+
+-(void)notifiedglkmove:(NSNotification*)notification
+{
+    if(self.changed)
+    {
+        self.changed = NO;
+        [self.spatiallifebar render];
+    }
 }
 
 @end
