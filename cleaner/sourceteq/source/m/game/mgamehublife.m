c249ea44df650a8b5e83a82244326f011dc3708a
vauxhall <zero.griffin@gmail.com>
2016-04-06 13:19:44 -0500

    model life

--- cleaner/sourceteq/source/m/game/mgamehublife.m
+++ cleaner/sourceteq/source/m/game/mgamehublife.m
@@ -1,8 +1,18 @@
 #import "mgamehublife.h"
 
 static NSInteger const initialamount = 100;
+static NSInteger const lifex = 20;
+static NSInteger const lifey = 20;
+static NSInteger const lifewidth = 200;
+static NSInteger const lifeheight = 20;
+static NSInteger const lifemarginhr = 2;
+static NSInteger const lifemarginvr = 2;
 
 @implementation mgamehublife
+{
+    NSInteger maxlifewidth;
+    NSInteger maxlifeheight;
+}
 
 -(instancetype)init:(mgame*)model
 {
@@ -10,6 +20,8 @@ static NSInteger const initialamount = 100;
     self.model = model;
     self.amount = initialamount;
     self.changed = NO;
+    maxlifeheight = lifeheight - (lifemarginvr + lifemarginvr);
+    maxlifewidth = lifewidth - (lifemarginhr + lifemarginhr);
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notifiedglkmove:) name:notification_glkmove object:nil];
     
@@ -30,6 +42,13 @@ static NSInteger const initialamount = 100;
         self.changed = NO;
         [self.spatiallifebar render];
     }
+}
+
+#pragma mark functionality
+
+-(void)reposlife
+{
+    
 }
 
 @end
