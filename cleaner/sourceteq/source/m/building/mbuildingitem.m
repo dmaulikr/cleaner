abd5479342eb86ea3f336fb56dd28389f5271476
vauxhall <zero.griffin@gmail.com>
2016-04-07 16:44:10 -0500

    model building

--- cleaner/sourceteq/source/m/building/mbuildingitem.m
+++ cleaner/sourceteq/source/m/building/mbuildingitem.m
@@ -1,18 +1,5 @@
 #import "mbuildingitem.h"
-#import "appdel.h"
 
 @implementation mbuildingitem
-
--(instancetype)init:(mtextures*)modeltextures dictionary:(NSDictionary*)dictionary
-{
-    self = [super init];
-    self.modeltextures = modeltextures;
-    self.assetname = dictionary[@"asset"];
-    self.spatial = [[gbuilding alloc] init:self];
-    self.spatial.width = [dictionary[@"width"] floatValue];
-    self.spatial.height = [dictionary[@"height"] floatValue];
-    
-    return self;
-}
 
 @end
