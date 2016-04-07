9f87a88b4c760c32a2271f864f9414b3a6245b10
vauxhall <zero.griffin@gmail.com>
2016-04-07 15:43:52 -0500

    model foes

--- cleaner/sourceteq/source/m/foe/mfoeiteminvader.m
+++ cleaner/sourceteq/source/m/foe/mfoeiteminvader.m
@@ -1,5 +1,11 @@
 #import "mfoeiteminvader.h"
 
+static NSInteger const width = 15;
+static NSInteger const height = 15;
+static NSInteger const life = 8;
+static NSInteger const damage = 3;
+static NSUInteger const score = 5;
+
 @implementation mfoeiteminvader
 
 -(instancetype)init:(mfoe*)model x:(NSInteger)x
--- cleaner/sourceteq/source/m/foe/mfoeitemocto.m
+++ cleaner/sourceteq/source/m/foe/mfoeitemocto.m
@@ -1,11 +1,3 @@
-//
-//  mfoeitemocto.m
-//  cleaner
-//
-//  Created by zero on 4/7/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mfoeitemocto.h"
 
 @implementation mfoeitemocto
