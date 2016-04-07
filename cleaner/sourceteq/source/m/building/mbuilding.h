367b7594a7e7abfc23c645eae8a097422ce4343b
vauxhall <zero.griffin@gmail.com>
2016-04-07 17:43:18 -0500

    strategy building

--- cleaner/sourceteq/source/m/building/mbuilding.h
+++ cleaner/sourceteq/source/m/building/mbuilding.h
@@ -1,9 +1,11 @@
 #import "appdel.h"
 
+@class sbuilding;
 @class mbuildingitem;
 
 @interface mbuilding:NSObject
 
 @property(strong, nonatomic)NSMutableArray<mbuildingitem*> *items;
+@property(strong, nonatomic)sbuilding *strategy;
 
 @end
--- cleaner/sourceteq/source/m/building/mbuilding.m
+++ cleaner/sourceteq/source/m/building/mbuilding.m
@@ -5,29 +5,9 @@
 -(instancetype)init:(mtextures*)modeltextures area:(marea*)modelarea
 {
     self = [super init];
-    self.modeltextures = modeltextures;
-    self.modelarea = modelarea;
-    self.items = [NSMutableArray array];
-    self.rawbuldings = [NSArray arrayWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"buildings" withExtension:@"plist"]];
-
-    [self addbuilding:50];
-    [self addbuilding:400];
+    self.strategy = [[sbuilding alloc] init:self];
     
     return self;
-}
-
-#pragma mark public
-
--(void)addbuilding:(CGFloat)x
-{
-    NSUInteger index = arc4random_uniform((CGFloat)self.rawbuldings.count);
-    NSDictionary *rawbuilding = self.rawbuldings[index];
-    mbuildingitem *building = [[mbuildingitem alloc] init:self.modeltextures dictionary:rawbuilding];
-    building.spatial.x = x;
-    building.spatial.y = self.modelarea.screenheight - building.spatial.height;
-    [building.spatial rasterize];
-    
-    [self.items addObject:building];
 }
 
 @end
